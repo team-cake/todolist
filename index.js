3613a8e7cd5ef5d5160564606dcb993a4ff3843f
team-cake <eka.cheuk@gmail.com>
2020-08-04 13:33:17 +0200

    Added POST, GET & PUT requests for lists of specific user

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const TodoList = require('./models').todoList
 
 app.use(express.json())
 
+// find user
 app.get('/users/:userId', async (req, res) => {
 	const userId = parseInt(req.params.userId)
 	const user = await User.findByPk(userId)
@@ -18,11 +19,12 @@ app.get('/users/:userId', async (req, res) => {
 	}
 })
 
+// create user
 app.post('/users', async (req, res) => {
 	try {
 		const email = req.body.email
 		if (!email || email === ' ') {
-			res.status(400).send('Must provide an email address')
+			res.status(400).send('Please provide an email address')
 		} else {
 			const user = await User.create(req.body)
 			res.json(user)
@@ -32,6 +34,7 @@ app.post('/users', async (req, res) => {
 	}
 })
 
+// update existing user
 app.put('/users/:userId', async (req, res, next) => {
 	try {
 		const userId = parseInt(req.params.userId)
@@ -47,11 +50,13 @@ app.put('/users/:userId', async (req, res, next) => {
 	}
 })
 
+// get todolists
 app.get('/todoLists', async (req, res, next) => {
 	const todoLists = await TodoList.findAll()
 	res.json(todoLists)
 })
 
+// create todolists
 app.post('/todoLists', async (req, res, next) => {
 	try {
 		const todoLists = await TodoList.create(req.body)
@@ -61,6 +66,7 @@ app.post('/todoLists', async (req, res, next) => {
 	}
 })
 
+// update todolists
 app.put('/todoLists/:todoListId', async (req, res, next) => {
 	try {
 		const todoListId = parseInt(req.params.todoListId)
@@ -75,6 +81,57 @@ app.put('/todoLists/:todoListId', async (req, res, next) => {
 		next(e)
 	}
 })
+
+// get lists of a user
+app.get('/users/:userId/lists', async (req, res, next) => {
+	try {
+		const userId = parseInt(req.params.userId)
+		const user = await User.findByPk(userId, {
+			include: [TodoList],
+		})
+		if (!user) {
+			res.status(404).send('List not found for this user')
+		} else {
+			res.send(user.todoLists)
+		}
+	} catch (e) {
+		next(e)
+	}
+})
+
+// create lists for a specific user, dont forget userid!
+app.post('/users/:userId/lists', async (req, res, next) => {
+	try {
+		const userId = parseInt(req.params.userId)
+		const user = await User.findByPk(userId)
+		if (!user) {
+			res.status(404).send('User not found')
+		} else {
+			const newList = await TodoList.create({ userId, ...req.body })
+			res.json(newList)
+		}
+	} catch (e) {
+		next(e)
+	}
+})
+
+// update list for specific user
+app.put('/users/:userId/lists/:listId', async (req, res, next) => {
+	try {
+		const listId = parseInt(req.params.listId)
+		const toUpdate = await TodoList.findByPk(listId)
+		if (toUpdate) {
+			const updated = await toUpdate.update(req.body)
+			res.json(updated)
+		} else {
+			res.status(404).send('List not found')
+		}
+	} catch (e) {
+		next(e)
+	}
+})
+
+
 
 function onListen() {
 	console.log(`Let's listen to :${PORT}`)
